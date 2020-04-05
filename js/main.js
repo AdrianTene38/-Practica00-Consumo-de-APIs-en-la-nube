48586f4d15dbee4a49ec5a16ef1718ec2745d310
adriantene <46764736+AdrianTene38@users.noreply.github.com>
2020-04-05 17:50:31 -0500

    cambio

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 var prueba;
 var result="";
-
+var req = new XMLHttpRequest();
 function bus(){
     var porNombre=document.getElementsByName("q")[0].value;
         document.getElementById("resultado").innerHTML=porNombre;
+       // Creación de la petición HTTP
+
+// Petición HTTP GET síncrona hacia el archivo fotos.json del servidor
+req.open("GET", "http://www.omdbapi.com/?apikey=eabd474&s="+porNombre, false);
+// Envío de la petición
+req.send(null);
+// Impresión por la consola de la respuesta recibida desde el servidor
+console.log(req.responseText);
 }
+/* http://www.omdbapi.com/?apikey=eabd474&s djfsdjk*/
