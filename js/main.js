0a32105e579f9ed7849999f484ffbfa067b4f19e
adriantene <46764736+AdrianTene38@users.noreply.github.com>
2020-04-08 16:06:15 -0500

    cambio

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ var result = "";
 var req = new XMLHttpRequest();
 
 function bus() {
-
+   
     var result = "";
-    var req = new XMLHttpRequest();
+    
     var porNombre = document.getElementsByName("q")[0].value;
     var numero_pag = document.getElementById("paginas").value;
     //document.getElementById("resultado").innerHTML=porNombre;
@@ -13,15 +13,16 @@ function bus() {
 
     // Petición HTTP GET síncrona hacia el archivo fotos.json del servidor
     req.open("GET", "http://www.omdbapi.com/?apikey=eabd474&s="
- + porNombre +  "&" + "page=" +  numero_pag, false);
+ + porNombre , false);
     // Envío de la petición
     req.send(null);
-
     var data = JSON.parse(req.responseText)
+
+    
     
     var detalles = "";
  
-    req.open("GET", "http://www.omdbapi.com/?apikey=eabd474&s="+ porNombre +  "&" + "page=" +  numero_pag, false);
+   
 
     if (porNombre == "") {
         detalles = "<tr>" +
@@ -29,7 +30,13 @@ function bus() {
             "</tr>";
         document.getElementById("informacion").innerHTML = detalles;
     } else {
-        data.Search.forEach(movie => {
+
+        
+ 
+      
+
+       data.Search.forEach(movie => {
+        
             detalles += "<tr>" +
                 "<td><a href='#' onclick=\" "+  movie.imdbID + "')\">Mas detalles </td>" +
                 "<td>" + movie.Title + "</td>" +
@@ -38,32 +45,56 @@ function bus() {
                 "<td><img src=" + movie.Poster + "</td>" +
                 "</tr>";
         });
+        
         document.getElementById("informacion").innerHTML = detalles;   
+        
     }
-   
+  
     $(document).ready(function () {
-        var pageNum ="";
-        var rowsShown = 5;
+        
+        var rowsShown = 10;
         var rowsTotal = $('#mia tbody tr').length;
-        var numPages = rowsTotal / rowsShown;
-        for (i = 0; i < numPages; i++) {
-            pageNum = i + 1;
-            $('.menu').append('<a href="#" rel="' + i + '">' + pageNum + '</a> ');
-        }
+        var numPages = data.totalResults / 10;
+      
+          // Envío de la petición
+               
         $('#mia tbody tr').hide();
         $('#mia tbody tr').slice(0, rowsShown).show();
         $('.menu a:first').addClass('active');
         $('.menu a').bind('click', function () {
+            
             $('.menu a').removeClass('active');
             $(this).addClass('active');
             var currPage = $(this).attr('rel');
             var startItem = currPage * rowsShown;
             var endItem = startItem + rowsShown;
+            data.Search.forEach(movie => {
+        
+                detalles += "<tr>" +
+                    "<td><a href='#' onclick=\" "+  movie.imdbID + "')\">Mas detalles </td>" +
+                    "<td>" + movie.Title + "</td>" +
+                    "<td>" + movie.Year + "</td>" +
+                    "<td>" + movie.Type + "</td>" +
+                    "<td><img src=" + movie.Poster + "</td>" +
+                    "</tr>";
+            });
+            document.getElementById("informacion").innerHTML = detalles;   
+           
+       
+
+        });
+            
             $('#mia tbody tr').css('opacity', '0.0').hide().slice(startItem, endItem).
                 css('display', 'table-row').animate({ opacity: 1 }, 300);
-        });
-        rowsTotal = "";
+
+       
+               
+               
+   
+            });
+        }
+       
     });
 
-
 }
+
